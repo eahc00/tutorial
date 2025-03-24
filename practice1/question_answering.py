138b16891b46a0261ea784b7c07629577e49039d
eahc00 <eahc00@naver.com>
2025-03-24 11:48:59 +0000

    출력 형식 수정

--- practice1/question_answering.py
+++ practice1/question_answering.py
@@ -6,7 +6,6 @@ import torch
 from pymilvus import connections, MilvusClient
 
 from retrieval import (
-    make_query_vector,
     create_hybrid_request,
     search_hybrid_collection,
 )
@@ -29,9 +28,11 @@ def create_prompt(question, docs):
     system_prompt = """ 당신은 대학생의 학습을 도와주는 학습 도우미입니다.
     사용자가 물어보는 질문에 대한 답을 학생이 제공한 문서에서 찾아 제공합니다. 
     문서에 적힌 내용만을 답하고 없으면 문서에서 찾을 수 없는 내용이라고 대답하세요.
-    존댓말로 친절하게 대답하여야 합니다."""
+    존댓말로 친절하게 대답하여야 합니다. """
 
-    user_text = system_prompt + f"\n\n문서: {docs} \n\n질문: {question}"
+    user_text = (
+        system_prompt + f"\n\n문서: {docs[0][0]['entity']['text']} \n\n질문: {question}"
+    )
 
     messages = [{"role": "user", "content": user_text}]
 
@@ -40,6 +41,7 @@ def create_prompt(question, docs):
 
 def answer_question(question, collection_name, device):
     docs = retrieve_docs(question, collection_name, device)
+    subject = docs[0][0]["entity"]["subject"]
 
     model_pipe = pipeline(
         "text-generation",
@@ -52,16 +54,25 @@ def answer_question(question, collection_name, device):
     prompt = create_prompt(question, docs)
     response = model_pipe(prompt, max_new_tokens=512)
 
-    return response[0]["generated_text"][-1]["content"]
+    return f"과목 : {subject}\n" + response[0]["generated_text"][-1]["content"]
 
 
 def main():
     device = "cuda:2" if torch.cuda.is_available() else "cpu"
     collection_name = "md_collection"
 
-    question = "기존 RNN의 문제점은 무엇인가요?"
+    ## ---------- NLP ------------ ##
+    # question = "기존 RNN의 문제점은 무엇인가요?"
     # question = "S2S의 예시로는 뭐가 있나요?"
     # question = "표현학습이란 무엇인가요?"
+    # question = "LSTM에 대해 설명해주세요"
+
+    ## ---------- Network ------------ ##
+    # question = "HTTP에 대해 설명해주세요"
+    # question = "보안의 세 가지 목표가 무엇인가요?"
+    # question = "NAT에 대해 자세히 설명해주세요"
+    question = "ARP에 대해 자세히 설명해주세요"
+
     docs = retrieve_docs(question, collection_name, device)
 
     answer = answer_question(question, collection_name, device)
